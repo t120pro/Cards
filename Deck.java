009381fc6126cd08b29defb1b6b20ae56de847b0
t120pro <60326519+t120pro@users.noreply.github.com>
2020-08-28 17:50:11 -0400

    Create Deck.java

---
+++ Deck.java
@@ -0,0 +1,53 @@
+package cardGame;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+	
+	public ArrayList<Card> deck = new ArrayList<Card>();
+	
+	public Deck(){
+		//builds a deck of cards (52 cards)
+		String[] suits = {"Spades", "Clubs", "Diamonds", "Hearts"};
+		String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+		for(String suit : suits){
+			for(String value : values) {
+			deck.add(new Card(value, suit));	
+			}
+		}
+	}
+	public Deck(String empty) {
+		
+	}
+	
+	public void addToDeck(String value, String suit) {
+		deck.add(new Card(value, suit));
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(deck);
+	}
+	
+	public Card drawCard() {
+		Card popped = deck.remove(deck.size() -1);
+		return popped;
+	}
+	
+	public String toString() {
+		String deckString = "[";
+		for (int i = 0; i < deck.size(); i++) {
+			Card currentCard = deck.get(i);
+			deckString += "/' " + currentCard + " /'";
+		}
+		deckString += "]";
+		return deckString;
+	}
+	
+	public static void main(String[] args) {
+		Deck deck1 = new Deck();
+		System.out.println(deck1.deck.size());
+		deck1.drawCard();
+		System.out.println(deck1.deck.size());
+	}
+}
